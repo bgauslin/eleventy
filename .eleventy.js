f1ad21b5d49743c09d10e5f679b5477e9458a28c
Ben Gauslin <github@gauslin.com>
2023-02-02 15:25:02 -0600

    static/copied stuff added to config; htaccess added

--- .eleventy.js
+++ .eleventy.js
@@ -2,11 +2,20 @@ const pluginRev = require('eleventy-plugin-rev');
 const sass = require('eleventy-sass');
 
 module.exports = (eleventyConfig) => {
+  // Static assets.
+  eleventyConfig.addPassthroughCopy('src/.htaccess');
+  eleventyConfig.addPassthroughCopy('src/apple-touch-icon.png');
+  eleventyConfig.addPassthroughCopy('src/favicon.svg');
+  eleventyConfig.addPassthroughCopy('src/files');
+  eleventyConfig.addPassthroughCopy('src/images');
+
+  // Stylesheet.
   eleventyConfig.addPlugin(pluginRev);
   eleventyConfig.addPlugin(sass, {
     rev: true,
   });
 
+  // Directory overrides.
   return {
     dir: {
       input: 'src',
