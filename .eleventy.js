a2cd526d04daa407106afa5faeae65e01d7fc7fd
Ben Gauslin <github@gauslin.com>
2024-01-31 12:25:37 -0600

    nunjucks wired up for DRYer templating

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,22 @@
+const Nunjucks = require('nunjucks');
 const pluginRev = require('eleventy-plugin-rev');
 const sass = require('eleventy-sass');
 
 module.exports = (eleventyConfig) => {
+  // Use nunjucks for templating.
+  let nunjucksEnvironment = new Nunjucks.Environment(
+      new Nunjucks.FileSystemLoader('src/_includes'));
+  eleventyConfig.setLibrary('njk', nunjucksEnvironment);
+
   // Static files.
   eleventyConfig.addPassthroughCopy('src/.htaccess');
   eleventyConfig.addPassthroughCopy('src/apple-touch-icon.png');
   eleventyConfig.addPassthroughCopy('src/favicon.svg');
-
-  // Images.
   eleventyConfig.addPassthroughCopy({'src/img/*.*': 'img'});
 
   // Sass stylesheet.
   eleventyConfig.addPlugin(pluginRev);
-  eleventyConfig.addPlugin(sass, {
-    rev: true,
-  });
+  eleventyConfig.addPlugin(sass, {rev: true});
 
   // Configuration options.
   return {
@@ -22,6 +24,7 @@ module.exports = (eleventyConfig) => {
       input: 'src',
       output: 'dist',
       layouts: '_layouts',
-    }
+    },
+    markdownTemplateEngine: 'njk',
   };
 };
