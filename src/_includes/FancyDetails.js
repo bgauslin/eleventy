613959aca622d225c7e1974046198b4a236dcf82
Ben Gauslin <github@gauslin.com>
2024-01-31 09:59:39 -0600

    fancy-details as local custom element; scss file renaming/organizing

--- src/_includes/FancyDetails.js
+++ src/_includes/FancyDetails.js
@@ -1,60 +1,41 @@
 /**
- * Extends all <details> elements in the DOM with open/closing transitions.
+ * Extends <details> elements with open/close transitions.
  */
 class FancyDetails extends HTMLElement {
   constructor() {
     super();
-    this.clickListener = this.handleClick.bind(this);
   }
 
   connectedCallback() {
-    this.updateElements();
-    document.body.addEventListener('click', this.clickListener);
+    this.addEventListener('click', this.handleClick);
   }
 
   disconnectedCallback() {
-    document.body.removeEventListener('click', this.clickListener);
+    this.removeEventListener('click', this.handleClick);
   }
 
   /**
-   * Adds/removes className on each <details> element to enable/disable
-   * CSS transitions.
-   */ 
-  updateElements() {
-    this.details = document.querySelectorAll('details');
-    if (this.details && this.details.length) {
-      for (const details of this.details) {
-        details.classList.add('fancy');
-      }
-    }
-  }
-
-  /**
-   * Gets current <details> element and updates CSS vars and HTML attributes
-   * for animated/accordion UX.
+   * Set CSS var scoped directly on <details> element if it's open; otherwise
+   * removes the CSS var and fallback to default value.
+   * If element is already open, hijack click so that arttribute is removed
+   * after the transition. Otherwise, default display is none for content
+   * when 'open' attribute is removed and we want to prevent that until the
+   * after the transition ends.
    */ 
   handleClick(event) {
     if (event.target.tagName !== 'SUMMARY') {
       return;
     }
 
-    const details = event.target.closest('details');
+    const element = event.target.closest('details');
 
-    // Set CSS var scoped directly on <details> element if it's open;
-    // otherwise remove the CSS var and fallback to default value.
-    // If element is already open, hijack click so that arttribute is removed
-    // after the transition. Otherwise, default display is none for content
-    // when 'open' attribute is removed and we want to prevent that until the
-    // after the transition ends.
-    if (details.open) {
+    if (element.open) {
       event.preventDefault();
-      details.style.removeProperty('--height');
-      details.addEventListener('transitionend', () => {
-        details.open = false;
-      }, {once: true});
+      element.style.removeProperty('--height');
+      element.addEventListener('transitionend', () => element.open = false, {once: true});
     } else {
       window.requestAnimationFrame(() => {
-        details.style.setProperty('--height', `${details.scrollHeight}px`);
+        element.style.setProperty('--height', `${element.scrollHeight}px`);
       });
     }
   }
--- src/content/index.md
+++ src/content/index.md
@@ -10,36 +10,38 @@ view: home
 
 <small>Architect · UXE · Musician</small>
 
-<details>
-  <summary>About</summary>
+<fancy-details>
+  <details>
+    <summary>About</summary>
 
-  Hello, I’m a former Chicago architect and ex-Google engineer who currently lives in New Orleans.
-  
-  When I’m not renovating my 140-year old house in the Vieux Carré, I spend quality time with friends, family, and the electric bass.
+    Hello, I’m a former Chicago architect and ex-Google engineer who currently lives in New Orleans.
+    
+    When I’m not renovating my 140-year old house in the Vieux Carré, I spend quality time with friends, family, and the electric bass.
 
-</details>
+  </details>
+</fancy-details>
 
-<details>
-  <summary>Projects</summary>
+<fancy-details>
+  <details>
+    <summary>Projects</summary>
 
-  Photos, sketches, and drawings of architectural projects alongside web apps made with TypeScript, Sass, and Web Components, all in no particular order.
+    Photos, sketches, and drawings of architectural projects alongside web apps made with TypeScript, Sass, and Web Components, all in no particular order.
 
-  {% include 'projects.njk' %}
+    {% include 'projects.njk' %}
 
-</details>
+  </details>
+</fancy-details>
 
-<details>
-  <summary>Contact</summary>
+<fancy-details>
+  <details>
+    <summary>Contact</summary>
 
-  You can reach me via email, text, or voicemail:
+    You can reach me via email, text, or voicemail:
 
-  {% include 'links.njk' %}
+    {% include 'links.njk' %}
 
-</details>
-
-<div>
-  <fancy-details></fancy-details>
-</div>
+  </details>
+</fancy-details>
 
 <script>
   {% include 'FancyDetails.js' %}
