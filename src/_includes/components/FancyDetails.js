35651dd966409d95e6ec86c9f73b15fa00982b2e
Ben Gauslin <github@gauslin.com>
2024-03-12 18:43:30 -0500

    refactors with fancyOpen and fancyClose methods

--- src/_includes/components/FancyDetails.js
+++ src/_includes/components/FancyDetails.js
@@ -24,41 +24,56 @@ class FancyDetails extends HTMLElement {
     if (event.target.tagName !== 'SUMMARY') {
       return;
     }
-
+    
     const details = event.target.closest('details');
-
+    
     if (details.open) {
       // Hijack click since 'open' attribute needs to be set after closing
       // transition ends. Immediately removing 'open' attribute sets
       // contents' display to 'none' and we don't want that just yet.
       event.preventDefault();
-
-      // Set var from 'auto' to pixel value for height transition to occur.
-      details.style.setProperty(this.sizeProp, `${details.scrollHeight}px`);
-
-      // Remove var on next tick and set data-attribute to trigger transition.
-      window.requestAnimationFrame(() => {
-        details.style.removeProperty(this.sizeProp);
-        details.dataset.closing = '';
-
-        // Remove 'open' and 'data-closing' attributes after transition ends.
-        details.addEventListener('transitionend', () => {
-          details.open = false;
-          delete details.dataset.closing;
-        }, {once: true});
-      });
+      this.fancyClose(details);
     } else {
-      window.requestAnimationFrame(() => {
-        // Set height var on next tick to trigger opening transition.
-        details.style.setProperty(this.sizeProp, `${details.scrollHeight}px`);
-
-        // After transition ends, set var's value to 'auto' which ensures
-        // <details> has fluid height on resize.
-        details.addEventListener('transitionend', () => {
-          details.style.setProperty(this.sizeProp, 'auto');
-        }, {once: true});
-      });
+      this.fancyOpen(details);
     }
+  }
+  
+  /**
+   * Closes a <details> element with a transition.
+   * @param {HTMLDetailsElement} element
+   */
+  fancyClose(element) {
+    // Set var from 'auto' to pixel value for height transition to occur.
+    element.style.setProperty(this.sizeProp, `${element.scrollHeight}px`);
+
+    // Remove var on next tick and set data-attribute to trigger transition.
+    window.requestAnimationFrame(() => {
+      element.style.removeProperty(this.sizeProp);
+      element.dataset.closing = '';
+
+      // Remove 'open' and 'data-closing' attributes after transition ends.
+      element.addEventListener('transitionend', () => {
+        element.open = false;
+        delete element.dataset.closing;
+      }, {once: true});
+    });
+  }
+
+  /**
+   * Opens a <details> element with a transition..
+   * @param {HTMLDetailsElement} element
+   */
+  fancyOpen(element) {
+    window.requestAnimationFrame(() => {
+      // Set height var on next tick to trigger opening transition.
+      element.style.setProperty(this.sizeProp, `${element.scrollHeight}px`);
+
+      // After transition ends, set var's value to 'auto' which ensures
+      // <details> has fluid height on resize.
+      element.addEventListener('transitionend', () => {
+        element.style.setProperty(this.sizeProp, 'auto');
+      }, {once: true});
+    });
   }
 }
 
