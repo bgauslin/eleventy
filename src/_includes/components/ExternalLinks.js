7ce9703151dcfc745d4f5b4b95f2ef12fa132dda
Ben Gauslin <github@gauslin.com>
2024-03-14 14:04:08 -0500

    adds ExternalLinks custom element; fixes renamed project property

---
+++ src/_includes/components/ExternalLinks.js
@@ -0,0 +1,78 @@
+/**
+ * Updates external links with attributes that open each link in a new
+ * tab/window, and removes the attributes when disconnected or disabled.
+ */	
+class ExternalLinks extends HTMLElement {
+	constructor() {
+		super();
+	}
+	
+	static observedAttributes = ['disabled'];
+	
+	connectedCallback() {
+		this.disabled = this.hasAttribute('disabled');
+		this.links = [...document.querySelectorAll('a')];
+
+		if (!this.disabled) {
+			this.updateLinks();
+		}
+	}
+	
+	attributeChangedCallback(name, oldValue, newValue) {
+		if (name === 'disabled') {
+			if (newValue === '') {
+				this.restoreLinks();
+			}
+
+			if (oldValue === '') {
+				this.disabled = true;
+				this.updateLinks();
+			}
+		}
+	}
+	
+	disconnectedCallback() {
+		this.restoreLinks();
+	}
+	
+	/**
+	 * Updates links with attributes for opening in new window/tab, along with
+	 * data-attribute for indicating it was updated by this custom element.
+	 * Email, phone, and text message links are never updated.
+	 */
+	updateLinks() {
+		const url = new URL(window.location);
+
+		for (const link of this.links) {
+			if (['mailto', 'sms', 'tel'].includes(link.protocol.replace(':', ''))) {
+				return;
+			}
+
+			if (link.hostname !== url.hostname) {
+				link.target = '_blank';
+				link.rel = 'noopener';
+        link.classList.add('external');
+			}
+		}
+	}
+	
+	/**
+	 * Removes attributes from links that were previously updated by this
+	 * custom element.
+	 */
+	restoreLinks() {
+		if (!this.links) {
+			return;
+		}
+
+		for (const link of this.links) {
+			if (link.classList.contains('external')) {
+				link.removeAttribute('target');
+				link.removeAttribute('rel');
+				link.classList.remove('external');
+			}
+		}
+	}
+}
+
+customElements.define('external-links', ExternalLinks);
