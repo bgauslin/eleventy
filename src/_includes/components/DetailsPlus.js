beb4c20fc419d4d6d24365e515782b88e6cba313
Ben Gauslin <github@gauslin.com>
2024-03-14 21:07:23 -0500

    renames component to DetailsPlus, updates/fixes accordion enabled/disabled logic

--- src/_includes/components/DetailsPlus.js
+++ src/_includes/components/DetailsPlus.js
@@ -1,7 +1,8 @@
 /**
- * Extends <details> elements with open/close transitions.
+ * Extends <details> elements with open/close transitions and saved state via
+ * URL hash or localStorage.
  */
-class FancyDetails extends HTMLElement {
+class DetailsPlus extends HTMLElement {
   constructor() {
     super();
   }
@@ -18,8 +19,15 @@ class FancyDetails extends HTMLElement {
     if (name === 'accordion') {
       this.accordion = this.hasAttribute('accordion');
 
+      // Accordion disabled; remove URL hash and default to localStorage.
       if (oldValue === '' && !newValue) {
-        history.replaceState(null, '', '.');
+        this.updateURL();
+        this.saveState();
+      }
+
+      // Accordion enabled: delete localStorage and use URL hash.
+      if (newValue === '' && !oldValue) {
+        localStorage.removeItem(this.storageItem);
       }
     }
   }
@@ -36,12 +44,50 @@ class FancyDetails extends HTMLElement {
     this.allDetails = [...this.querySelectorAll('details')];
     this.sizeProp = '--block-size';
     this.storageItem = 'open';
-    this.url = new URL(window.location);
     
     for (const details of this.allDetails) {
       if (details.open) {
         details.style.setProperty(this.sizeProp, 'auto');
       }
+    }
+  }
+
+  /**
+   * Restores 'open' state of <details> elements from URL hash for accordion
+   * behavior, or from localStorage otherwise. If there's a valid hash, but
+   * no accordion behavior, the element whose ID matches the hash will be
+   * open, and localStorage is reset accordingly.
+   */
+  restoreState() {
+    const url = new URL(window.location);
+    const hash = url.hash.replace('#', '');
+    const current = this.allDetails.find(element => element.id === hash);
+
+    // Open the <details> element whose ID matched URL hash.
+    if (current) {
+      current.style.setProperty(this.sizeProp, 'auto');
+      current.open = true;
+    
+    // Otherwise, get localStorage and open the elements from prior visit.
+    } else {
+      const saved = JSON.parse(localStorage.getItem(this.storageItem));
+      if (saved) {
+        for (const details of this.allDetails) {
+          if (saved.includes(details.id)) {
+            details.open = true;
+            details.style.setProperty(this.sizeProp, 'auto');
+          }
+        }
+      }
+    }
+
+    // Remove localStorage if accordion is enabled; otherwise, reset URL and
+    // save the single 
+    if (this.accordion) {
+      localStorage.removeItem(this.storageItem);
+    } else {
+      this.updateURL();
+      this.saveState();
     }
   }
 
@@ -55,22 +101,22 @@ class FancyDetails extends HTMLElement {
       return;
     }
     
-    const details = event.target.closest('details');
+    const target = event.target.closest('details');
     
     // Hijack click since 'open' attribute needs to be set after closing
     // transition ends. Immediately removing 'open' attribute sets
     // contents' display to 'none' and we don't want that just yet.
-    if (details.open) {
+    if (target.open) {
       event.preventDefault();
-      this.fancyClose(details);
+      this.fancyClose(target);
     } else {
-      this.fancyOpen(details);
+      this.fancyOpen(target);
 
       // Close other <details> elements if accordion behavior is enabled.
       if (this.accordion) {
-        for (const element of this.allDetails) {
-          if (details !== element && element.open) {
-            this.fancyClose(element);
+        for (const details of this.allDetails) {
+          if (details.open && details !== target) {
+            this.fancyClose(details);
           }
         }
       }
@@ -79,7 +125,7 @@ class FancyDetails extends HTMLElement {
   
   /**
    * Opens a <details> element with a transition.
-   * @param {HTMLDetailsElement} element
+   * @param {!HTMLDetailsElement} element
    */
   fancyOpen(element) {
     window.requestAnimationFrame(() => {
@@ -97,11 +143,11 @@ class FancyDetails extends HTMLElement {
 
   /**
    * Closes a <details> element with a transition.
-   * @param {HTMLDetailsElement} element
+   * @param {!HTMLDetailsElement} element
    */
   fancyClose(element) {
     if (this.accordion) {
-      history.replaceState(null, '', '.');
+      this.updateURL();
     }
 
     window.requestAnimationFrame(() => {
@@ -124,16 +170,15 @@ class FancyDetails extends HTMLElement {
   }
 
   /**
-   * Saves state of opened <details> elements to localStorage for restoring
-   * on page load.
-   * @param {HTMLDetailsElement} element
+   * Updates URL hash if accordion is enabled; otherwise saves a list of opened
+   * <details> elements to localStorage for restoring on page load.
+   * @param {?HTMLDetailsElement} element
    */
   saveState(element) {
-    if (this.accordion) {
+    if (element && this.accordion) {
       if (element.open) {
-        history.replaceState(null, '', `#${element.id}`);
+        this.updateURL(element.id);
       }
-      localStorage.removeItem(this.storageItem);
     } else {
       const saved = [];
       for (const details of this.allDetails) {
@@ -144,34 +189,16 @@ class FancyDetails extends HTMLElement {
       localStorage.setItem(this.storageItem, JSON.stringify(saved));
     }
   }
-  
+
   /**
-   * Restores 'open' state of <details> elements from URL hash for accordion
-   * behavior, or from localStorage otherwise. If there's a valid hash, but
-   * no accordion behavior, the element whose ID matches the hash will be
-   * open, regardless of its localStorage state.
+   * Helper method that updates browser history with hash in URL if an ID is
+   * provided; otherwise removes the hash (if it exists) and resets URL.
+   * @param {?string} id - <details> element ID
    */
-  restoreState() {
-    const hash = this.url.hash.replace('#', '');
-    const current = this.allDetails.find(element => element.id === hash);
-
-    if (current) {
-      current.style.setProperty(this.sizeProp, 'auto');
-      current.open = true;
-    }
-
-    if (!this.accordion && !current) {
-      const saved = JSON.parse(localStorage.getItem(this.storageItem));
-      if (saved) {
-        for (const details of this.allDetails) {
-          if (saved.includes(details.id)) {
-            details.open = true;
-            details.style.setProperty(this.sizeProp, 'auto');
-          }
-        }
-      }
-    }
+  updateURL(id) {
+    const hash = id ? `#${id}` : '.';
+    history.replaceState(null, '', hash);
   }
 }
 
-customElements.define('fancy-details', FancyDetails);
+customElements.define('details-plus', DetailsPlus);
