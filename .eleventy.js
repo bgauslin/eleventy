fca126cfd4ef5b1c2e7ad09aa63a74dc6a154c0f
Ben Gauslin <github@gauslin.com>
2025-03-20 10:39:30 +0100

    Update .eleventy.js
    See previous commit

--- .eleventy.js
+++ .eleventy.js
@@ -8,18 +8,6 @@ module.exports = (eleventyConfig) => {
       new Nunjucks.FileSystemLoader('src/_includes'));
   eleventyConfig.setLibrary('njk', nunjucksEnvironment);
 
-  // Custom ordering of slideshows on 'slides' landing page.
-  eleventyConfig.addCollection('slideshow', (collection) =>
-    collection
-      .getAll()
-      .filter((item) => {
-        return 'order' in item.data;
-      })
-      .sort((a, b) => {
-        return (a.data.order || 0) - (b.data.order || 0);
-      })
-  );
-
   // CSS minification.  
   eleventyConfig.addFilter('cssmin', (code) => {
     return new CleanCSS({}).minify(code).styles;
