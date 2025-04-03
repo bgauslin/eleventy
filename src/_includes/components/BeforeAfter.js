be72f02b90feb11d833686e816f96363330ad09c
Ben Gauslin <github@gauslin.com>
2025-04-03 14:42:12 +0200

    before-after button > p

--- src/_includes/components/BeforeAfter.js
+++ src/_includes/components/BeforeAfter.js
@@ -22,10 +22,12 @@ class BeforeAfter extends HTMLElement {
     this.attachShadow({mode: 'open'});
     this.shadowRoot.innerHTML = `
       <slot name="images"></slot>
-      <p>
-        <slot name="before">Click for view after</slot>
-        <slot name="after">Return to view before</slot>
-      </p>
+      <button type="button">
+        <span>
+          <slot name="before">Click for view after</slot>
+          <slot name="after">Return to view before</slot>
+        </span>
+      </button>
     `;
     this.before = this.shadowRoot.querySelector('[name=before]');
     this.after = this.shadowRoot.querySelector('[name=after]');
@@ -50,8 +52,8 @@ class BeforeAfter extends HTMLElement {
       }
 
       :host {
-        cursor: pointer;
         display: grid;
+        gap: .75rem;
         grid: 1fr auto / 1fr;
         inline-size: 100%;
       }
@@ -68,13 +70,42 @@ class BeforeAfter extends HTMLElement {
         z-index: 1;
       }
 
-      p {
-        font-size: small;
-        font-weight: bold;
-        grid-area: 2 / 1;
-        margin-block: .5rem 0;
-        opacity: var(--text-opacity);
-        text-align: center;
+      button {
+        appearance: none;
+        background-color: var(--fill-2);
+        border: none;
+        border-radius: 10rem;
+        color: var(--text-color);
+        cursor: pointer;
+        font: inherit;
+        font-size: .75em;
+        outline: none;
+        padding-block: .25rem;
+        padding-inline: 1rem;
+        place-self: center;
+        transition: background-color var(--transition), color var(--transition), transform var(--transition);
+        white-space: nowrap;
+      }
+      
+      button:focus-visible {
+        background-color: var(--text-color);
+        color: var(--fill-0);
+      }
+
+      @media (any-hover: hover) {
+        button:hover {
+          background-color: var(--text-color);
+          color: var(--fill-0);
+        }
+        
+        button:active {
+          transform: scale(.8);
+        }
+      }
+
+      button span {
+        opacity: .7;
+        pointer-events: none;
       }
     `);
     this.shadowRoot.adoptedStyleSheets = [styles];
