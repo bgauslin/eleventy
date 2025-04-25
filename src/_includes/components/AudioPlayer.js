83eb1729a734528112b6fa59b334a9ccc99ff5ac
Ben Gauslin <github@gauslin.com>
2025-04-26 00:32:45 +0200

    js and css moved to files

--- src/_includes/components/AudioPlayer.js
+++ src/_includes/components/AudioPlayer.js
@@ -1,88 +1,98 @@
+// Attach a <script> element for the YouTube API.
+const script = document.createElement('script');
+script.src = 'https://www.youtube.com/iframe_api';
+document.body.appendChild(script);
+
+// Get all the custom elment IDs for generating HTML and video objects.
+const elements = document.querySelectorAll('[data-video]');
+const ids = [...elements].map(element => element.dataset.video);
+
+// Inject elements for <iframe> replacement. The YouTube API doesn't like
+// multi-line template literals, hence the multiple lines of innerHTML.
+for (const [index, element] of elements.entries()) {
+  element.innerHTML = `<div id="${ids[index]}"></div>`;
+  element.innerHTML += `
+    <button title="Play">
+      ${renderIcon('play')}
+    </button>
+  `;
+}
+
+// Save references to all the buttons for updating their labels on click.
+const buttons = document.querySelectorAll('[data-video] > button');
+
+// Single listener for all clicks.
+const clickHandler = handleClick.bind(this);
+
+// Empty array for saving all the video objects created by the YouTube API.
+const videos = [];
+
 /**
- * Custom element that embeds a YouTube video of audis, adds controls, and
- * hides the video from view.
+ * Makes a new Player for each video, then add a 'click' handler for
+ * play/pause control over all videos.
  */
+function onYouTubeIframeAPIReady() {
+	for (const id of ids) {
+		const player = new YT.Player(id, {
+      videoId: id,
+			events: {
+        onReady: onReady,
+      },
+      playerVars: {
+        'controls': 0,
+        'enablejsapi': 1,
+        'fs': 0,
+        'playsinline': 1,
+      },
+		});
+	}
 
-// TODO: Review this GitHub post for multiple videos on a single page:
-// https://gist.github.com/bajpangosh/d322c4d7823d8707e19d20bc71cd5a4f
+  document.addEventListener('click', clickHandler);
+}
 
-customElements.define('audio-player', class extends HTMLElement {
-  constructor() {
-    super();
-    this.clickHandler = this.playPause.bind(this);
-    this.play = false;
-    this.videoId = this.getAttribute('embed');
-  }
+/**
+ * Saves all the generated video objects on the page for auto-toggling their
+ * play/pause state.
+ */
+function onReady(event) {
+  videos.push(event.target);
+}
 
-  connectedCallback() {
-    this.setup();
-    this.addEventListener('click', this.clickHandler);
-  }
+/**
+ * Gets the click target and updates the rest of the DOM if a play/pause
+ * <button> was clicked.
+ */ 
+function handleClick(event) {
+  const target = event.composedPath()[0];
 
-  disconnectedCallback() {
-    this.setup();
-    this.removeEventListener('click', this.clickHandler);
-  }
+  if (target.tagName.toLowerCase() !== 'button') return;
   
-  onReady() {
-    console.log('onReady', event);
+  const button = target;
+  const iframe = button.previousElementSibling || button.nextElementSibling;
+  const active = videos.find(video => video.g.id === iframe.id);
+
+  if (button.title === 'Play') {
+    videos.forEach(video => video.pauseVideo());
+    buttons.forEach(button => {
+      button.title = 'Play';
+      button.innerHTML = renderIcon('play');
+    });
+    active.playVideo();
+    button.title = 'Pause';
+    button.innerHTML = renderIcon('pause');
+  } else {
+    active.pauseVideo();
+    button.title = 'Play';
+    button.innerHTML = renderIcon('play');
   }
+}
+
+function renderIcon(type) {
+  const path = (type === 'play') ? 'M6,3 L6,21 L21,12 Z' : 'M6,4 L6,20 L10,20 L10,4 M14,4 L14,20 L18,20 L18,4';
   
-  onStateChange() {
-    console.log('onStateChange', event.data);
-
-      // -1  unstarted
-      //  0  ended
-      //  1  playing
-      //  2  paused
-      //  3  buffering
-      //  5  video cued
-  }
-  
-  playPause(event) {
-    if (event.target !== this.button) return;
-
-    this.play = !this.play;
-    this.button.textContent = this.play ? 'Pause' : 'Play';
-    
-    if (this.play) {
-      this.player.playVideo();
-    } else {
-      this.player.pauseVideo();
-    }
-  }
-
-  showPlayback() {
-    const interval = setInterval(() => {
-      const fraction = this.player.getVideoLoadedFraction();
-      const state = this.player.getPlayerState();
-      const time = this.player.getCurrentTime();
-      const duration = this.player.getDuration();
-    }, 1000);
-  }
-
-  setup() {
-    this.innerHTML = `
-      <div id="video"></div>
-      <button>Play</button>
-    `;
-
-    this.button = this.querySelector('button');
-    
-    if (!window.YT) {
-      const script = document.createElement('script');
-      script.src = 'https://www.youtube.com/iframe_api';
-      this.appendChild(script);
-    }
-
-    window.onYouTubeIframeAPIReady = () => {
-      this.player = new YT.Player('video', {
-        videoId: this.videoId,
-        events: {
-          'onReady': this.onReady.bind(this),
-          'onStateChange': this.onStateChange.bind(this),
-        },
-      });
-    }
-  }
-});
+  return `
+    <svg aria-hidden="true" viewbox="0 0 24 24">
+      <path d="${path}"/>
+    </svg>
+  `;
+}
